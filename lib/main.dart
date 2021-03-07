70dbfa29eff2a7e333858d7a20feae56ab64015f
demian <demian@kcd.co.kr>
2021-03-07 17:34:01 +0900

    Add TabBar and TabBarView to show different location information

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'AllMap',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -41,7 +42,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Stack(children: [
         Center(
-          child: Text('Maps is here',
+          child: Text('Map is here',
               style: Theme.of(context)
                   .textTheme
                   .headline4
@@ -65,35 +66,76 @@ class _MyHomePageState extends State<MyHomePage> {
             right: 0,
             bottom: 0,
             child: SafeArea(
-                child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Card(
-                elevation: 2.0,
-                child: Padding(
-                    padding: const EdgeInsets.only(right: 8),
-                    child: Column(
-                      children: [
-                        ListTile(
-                          leading: Icon(Icons.store),
-                          title: Text('Cafe Name'),
-                          subtitle: Text('Cate Description'),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
+              child: DefaultTabController(
+                  length: 3,
+                  child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Card(
+                        elevation: 2.0,
+                        child: Column(
                           children: [
-                            TextButton(
-                              child: Text('Show Details'),
-                              onPressed: () {
-                                print('"Show Details" is tapped');
-                              },
+                            TabBar(
+                              labelColor: Colors.black,
+                              unselectedLabelColor: Colors.grey,
+                              tabs: [
+                                Tab(child: Text('Goolge')),
+                                Tab(child: Text('Naver')),
+                                Tab(child: Text('Kakao')),
+                              ],
                             ),
+                            SizedBox(
+                              height: 120,
+                              child: TabBarView(children: [
+                                LocationCard(
+                                  title: 'Google Cafe name',
+                                  subTitle: "Google Cafe Subtitle",
+                                ),
+                                LocationCard(
+                                  title: 'Naver Cafe name',
+                                  subTitle: "Naver Cafe Subtitle",
+                                ),
+                                LocationCard(
+                                  title: 'Kakao Cafe name',
+                                  subTitle: "Kakao Cafe Subtitle",
+                                ),
+                              ]),
+                            )
                           ],
-                        )
-                      ],
-                    )),
-              ),
-            )))
+                        ),
+                      ))),
+            ))
       ]),
+    );
+  }
+}
+
+class LocationCard extends StatelessWidget {
+  final String title;
+  final String subTitle;
+
+  const LocationCard({@required this.title, @required this.subTitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          leading: Icon(Icons.store),
+          title: Text(title),
+          subtitle: Text(subTitle),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            TextButton(
+              child: Text('Show Details'),
+              onPressed: () {
+                print('"Show Details" is tapped');
+              },
+            ),
+          ],
+        )
+      ],
     );
   }
 }
